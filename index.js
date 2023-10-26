4f1fdbe5540fc9f0b59d868d17369d3d53389a0f
zanimail <120774114+zanimail@users.noreply.github.com>
2023-10-26 19:08:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+var token = "f92606fbfc9ce05a352e1bc2efcfa9ac86d7ba2f";
+var query = "москва хабар";
+
+var options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Token " + token
+    },
+    body: JSON.stringify({query: query})
+}
+
+
+
+function NewObjFromREST (value, unrestricted_value, street, region)
+{
+    return {
+    value: value,
+    unrestricted_value: unrestricted_value,
+    data :{
+        street: street,
+        region: region,
+    },
+    }
+}
+
+
+
+fetch(url, options)
+.then(response => response.json())
+.then(result =>  {
+    var ListResultOdject = []
+    for (let item of result.suggestions){
+        let NewObjFromREST_item = new NewObjFromREST(item.value, item.unrestricted_value, item.data.street, item.data.region);
+        ListResultOdject.push(NewObjFromREST_item);
+    }
+    return ListResultOdject;})
+.then(ListResultOdject => {
+    for (let item of ListResultOdject){
+        let JsonResultObj =JSON.stringify(item);
+        console.log(JsonResultObj);
+    }    
+})
+.catch(error => console.log("error", error));
+
+
+
+/*
+var NewObjFromREST_my = new NewObjFromREST(RESTObj.value, RESTObj.unrestricted_value, RESTObj.data.street, RESTObj.data.region);
+let srt =JSON.stringify(NewObjFromREST_my);
+let obj = JSON.parse(srt);
+console.log(obj);*/
+
